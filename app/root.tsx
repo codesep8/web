4f73f63f78f318e9e1ea77c4ca3385ae3aa825b9
Junyoung <codesep8@gmail.com>
2025-03-26 21:34:37 +0900

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -56,7 +56,7 @@ export async function loader({ request }: Route.LoaderArgs) {
     ? {
         isLoggedIn: true,
         userId: session.get("userId") as string,
-        userName: session.get("userName") as string
+        userName: session.get("username") as string
       }
     : {
         isLoggedIn: false,
